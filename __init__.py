47f5fd0304b638ca9f180efde997348c0ca025d8
The-Master777 <themaster777@outlook.com>
2015-05-03 14:31:03 +0200

    Added __init__.py file

---
+++ __init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Import selected classes to publish them in the module-scope
+# Theese can now easily be imported
+from EndpointScraper import BoxEndpointScraper
+from HybridApi import SpeedportHybridApi
